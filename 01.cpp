ddf427d38083c32d9162d8f9c6c7748969f6ffdb
MOHIT-IITP <codingbymohit@gmail.com>
2024-09-22 15:06:16 +0530

    done adding contest question

--- 01.cpp
+++ 01.cpp
@@ -1,19 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-vector<int> twoSum(vector<int>& nums, int target){
-  for(int i=0;i<nums.size()-1;i++){
-    for(int j=i+1;j<nums.size();j++){
-      if(nums[i]+nums[j]==target){
-        return {i,j};
+vector<int> twoSum(vector<int> &nums, int target) {
+  for (int i = 0; i < nums.size() - 1; i++) {
+    for (int j = i + 1; j < nums.size(); j++) {
+      if (nums[i] + nums[j] == target) {
+        return {i, j};
       }
     }
   }
   return {};
 }
 
-int main(){
-  vector<int> nums({2,7,10,15});
-  twoSum(nums,9);
+int main() {
+  vector<int> nums({2, 7, 10, 15});
+  twoSum(nums, 9);
   return 0;
 }
 // this code is done by mohit kumar
---
+++ 3298.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  long long validSubstringCount(string s, string p) {
+    int tc[26] = {0};
+    for (char c : p) {
+      tc[c - 'a']++;
+    }
+
+    int wc[26] = {0};
+    int l = 0;
+    int n = p.length();
+    long long res = 0;
+
+    for (int r = 0; r < s.length(); r++) {
+      char c = s[r];
+
+      if (tc[c - 'a'] > 0) {
+        if (wc[c - 'a'] < tc[c - 'a']) {
+          n--;
+        }
+      }
+      wc[c - 'a']++;
+      while (n == 0) {
+        res += s.length() - r;
+        char sc = s[l];
+        wc[sc - 'a']--;
+        if (tc[sc - 'a'] > 0 && wc[sc - 'a'] < tc[sc - 'a']) {
+          n++;
+        }
+        l++;
+      }
+    }
+    return res;
+  }
+};
+int main() { return 0; }
