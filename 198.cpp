cf7d654f6fa8311c140ce865d0219ab897f0b882
MOHIT-IITP <codingbymohit@gmail.com>
2024-10-04 19:58:33 +0530

    done question no 198 leetcode using dynamic programming

--- 198.cpp
+++ 198.cpp
@@ -1,21 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int N = 1e5+10;
+int dp[N];
+
+int func(int i , vector<int> & nums){
+    if(i<0) return 0;
+    if(dp[i] != -1) return dp[i];
+    dp[i] = max(nums[i] + func(i-2,nums), func(i-1, nums));
+    return dp[i];
+} 
+
 int rob(vector<int> & nums){
-  int n = nums.size();
-  if(n==1){
-    return nums[0];
-  }
-  vector<int> dp(n,0);
-  dp[0]= nums[0];
-  dp[1] = max(nums[0], nums[1]);
-  for(int i =2;i<n;i++){
-    dp[i]= max(dp[i-1], nums[i]+dp[i-2]);
-  }
-  return dp[n-1];
+    memset(dp, -1, sizeof(dp));
+    int n = nums.size();
+    return func(n-1, nums);
 }
 
-int main()
-{
-  return 0;
+
+int main(){
+    return 0;
 }
