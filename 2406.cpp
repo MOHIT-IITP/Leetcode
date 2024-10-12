43f101735953fc75d723fd383b09b7331500991c
MOHIT-IITP <codingbymohit@gmail.com>
2024-10-12 23:33:15 +0530

    done dcc leetcode

---
+++ 2406.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (const auto& interval : intervals) {
+            int start = interval[0], end = interval[1];
+            if (!pq.empty() && pq.top() < start) {
+                pq.pop();
+            }
+            pq.push(end);
+        }
+        return pq.size();
+    }
+};
