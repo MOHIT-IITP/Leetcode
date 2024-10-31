6f3fca623d5dd54c081751979e1cdf4eb20bc3fb
MOHIT-IITP <codingbymohit@gmail.com>
2024-10-31 21:13:27 +0530

    done qno 1448 leetcode

--- 450.cpp
+++ 450.cpp
@@ -1,53 +1,48 @@
-//  one of the difficult question 
-#include<bits/stdc++.h>
-using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == NULL)
+            return NULL;
 
-TreeNode* deleteNode(TreeNode* root, int key){
-  if(root==NULL){
-    return NULL;
-  }
-  if(root->val==key){
-    return helper(root);
-  }
-  TreeNode* dummy = root;
-  while(root!=NULL){
-    if(root->val>key){
-      if(root->left !=NULL && root->left->val == key){
-        root->left = helper(root->left);
-        break;
-      }else{
-        root = root->left;
-      }
-    }else{
-      if(root->right!=NULL && root->right->val == key){
-        root->right = helper(root->right);
-        break;
-      }else{
-        root = root->right;
-      }
+        if (root->val > key) {
+            root->left = deleteNode(root->left, key);
+        } else if (root->val < key) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            return helper(root);
+        }
+        
+        return root;
     }
-  }
-  return dummy;
-}
-TreeNode* helper(TreeNode* root){
-  if(root->left == NULL){
-    return root->right;
-  }else if(root->right == NULL){
-    return root->left;
-  }
-  TreeNode* rightchild = root->right;
-  TreeNode* lastchild = findlastchild(root->left);
-  lastchild->right = rightchild;
-  return root->left;
-}
-TreeNode* findlastchild(TreeNode* root){
-  if(root->right == NULL){
-    return root;
-  }
-  return findlastchild(root->right);
-}
 
-int main()
-{
-  return 0;
-}
+    TreeNode* helper(TreeNode* node) {
+        if (node->left == NULL)
+            return node->right;
+        if (node->right == NULL)
+            return node->left;
+
+        TreeNode* rightChild = node->right;
+        TreeNode* lastRight = findLast(node->left);
+        lastRight->right = rightChild;
+        return node->left;
+    }
+
+    TreeNode* findLast(TreeNode* node) {
+        while (node->right != NULL) {
+            node = node->right;
+        }
+        return node;
+    }
+};
+
