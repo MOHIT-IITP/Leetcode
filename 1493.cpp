ccc7a57e3d9fcbc46c8c9942b0d2eab161dd5b95
MOHIT-IITP <codingbymohit@gmail.com>
2024-11-11 19:46:11 +0530

    done qno 1493 leetcode

---
+++ 1493.cpp
@@ -0,0 +1,23 @@
+class Solution{
+    public:
+        int longestSubarray(vector<int> & nums){
+            int left = 0, zeros = 0, maxLength = 0;
+            int n = nums.size();
+            for(int right = 0; right < n ; ++right){
+                if(nums[right] == 0){
+                    zeros++;
+                }
+                while(zeros > 1){
+                    if(nums[left] == 0){
+                        zeros--;
+                    }
+                    left++;
+                }
+                maxLength = max(maxLength , right - left);
+            }
+            return maxLength;
+        }
+}
+int main(){
+    return 0;
+}
