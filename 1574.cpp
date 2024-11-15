5ce7fe0e40a08cf70c4d1bd6ef004a6964a92225
MOHIT-IITP <codingbymohit@gmail.com>
2024-11-15 15:42:54 +0530

    done dcc leetcode

---
+++ 1574.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+        int findLengthOfShortestSubarray(vector<int> & arr){
+            int n = arr.size();
+            int right = n-1;
+            while(right > 0 && arr[right] >= arr[right - 1]){
+                right--;
+            }
+            int ans = right;
+            int left = 0;
+            while(left < right && (left ==0 || arr[left - 1] <= arr[left])){
+                while(right < n && arr[left] > arr[right]){
+                    right++;
+                }
+                ans = min(ans, right - left + 1);
+                left++;
+            }
+            return ans;
+        }
+}
+int main(){
+    return 0;
+}
