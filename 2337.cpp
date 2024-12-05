6cb59769a02e25b065e5558840dadf825e234a40
MOHIT-IITP <codingbymohit@gmail.com>
2024-12-05 12:05:43 +0530

    done dcc leetcode

---
+++ 2337.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        if (start == target) {
+            return true;
+        }
+        int waitL = 0;
+        int waitR = 0;
+        
+        for (int i = 0; i < start.length(); i++) {
+            char curr = start[i];
+            char goal = target[i];
+            if (curr == 'R') {
+                if (waitL > 0) {
+                    return false;
+                }
+                waitR++;
+            }
+            if (goal == 'L') {
+                if (waitR > 0) {
+                    return false;
+                }
+                waitL++;
+            }
+            if (goal == 'R') {
+                if (waitR == 0) {
+                    return false;
+                }
+                waitR--;
+            }
+            if (curr == 'L') {
+                if (waitL == 0) {
+                    return false;
+                }
+                waitL--;
+            }
+        }
+        return waitL == 0 && waitR == 0;
+    }
+}
+int main(){
+    return 0;
+}
