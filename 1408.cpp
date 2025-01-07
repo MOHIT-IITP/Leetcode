c108096ab0ef5a658382bda0dca8e2ec404cf7ee
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-01-07 22:32:06 +0530

    done dcc leetcode

---
+++ 1408.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  vector<string> stringMatching(vector<string> &words) {
+    int n = words.size();
+    vector<string> ans;
+
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (i != j && words[j].find(words[i]) != string::npos) {
+          ans.push_back(words[i]);
+          break;
+        }
+      }
+    }
+
+    return ans;
+  }
+};
+int main() { return 0; }
