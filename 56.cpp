41b24abd385ba8996822cfe898e8e0638497e65e
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-01-29 18:02:11 +0530

    done qno 56 leetcode

--- 56.cpp
+++ 56.cpp
@@ -1,30 +1,22 @@
-// correct answer
-// the answer is correct but it is O(n2) , you have to do it in O(n)
-#include<bits/stdc++.h>
-#include<iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+  vector<vector<int>> merge(vector<vector<int>> &intervals) {
+    sort(intervals.begin(), intervals.end());
+    int k = 0;
 
-int max(vector<int> & nums){
-  int n = nums.size();
-  vector<int> another;
-  for(int i =0;i<n;i++){
-    int s= 0;
-    for(int j=i;j<n;j++){
-      s+=nums[j];
-      another.push_back(s);
+    for (int i = 1; i < intervals.size(); i++) {
+      if (intervals[k][1] >= intervals[i][0]) {
+        intervals[k][1] = max(intervals[k][1], intervals[i][1]);
+      } else {
+        k++;
+        intervals[k] = intervals[i];
+      }
     }
-  }
-  for(auto it: another){
-    cout<<it<<endl;
-  }
-  return *max_element(another.begin(),another.end());
-}
 
-int main()
-{
-  vector<int> nums({5,4,-1,7,8});
-  int result = max(nums);
-  cout<<result;
-  return 0;
-}
+    intervals.resize(k + 1);
+    return intervals;
+  }
+};
+int main() { return 0; }
