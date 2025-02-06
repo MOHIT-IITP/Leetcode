56bc4d080976ed70a2ecf02175ee93cd6f10619f
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-02-06 21:53:41 +0530

    done dcc leetcode

---
+++ 1726.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int tupleSameProduct(vector<int> &nums) {
+    unordered_map<int, int> mp;
+    int ans = 0, n = nums.size();
+    for (int i = 0; i < n; i++)
+      for (int j = i + 1; j < n; j++) {
+        int product = nums[i] * nums[j];
+        ans += 8 * mp[product];
+        mp[product]++;
+      }
+    return ans;
+  }
+};
+int main() { return 0; }
