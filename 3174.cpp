7cfd2a7201e1277395a913e8906b7973a62b3fff
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-02-10 22:13:31 +0530

    done dcc leetcode

---
+++ 3174.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  static string clearDigits(string &s) {
+    int n = s.size(), j = 0;
+    for (int i = 0; i < n; i++) {
+      if (isdigit(s[i]) && j > 0)
+        j--;
+      else
+        s[j++] = s[i];
+    }
+    s.resize(j);
+    return s;
+  }
+};
+int main() { return 0; }
