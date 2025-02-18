8fadbc689fe00a1e3824f439e88b04b6068c1fca
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-02-18 23:27:55 +0530

    done dcc

---
+++ 2375.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool match(string &num, string &pattern){
+        for(int i=0;i<pattern.length(); i++){
+            if((pattern[i] == 'I' && num[i] > num[i+1]) || 
+            (pattern[i] == 'D' && num[i] < num[i+1])){
+                return false;
+            }
+        }
+        return true;
+    }
+    string smallestNumber(string pattern) {
+        int n = pattern.length();
+        string num = "";
+        for(int i =1;i <= n+1; i++){
+            num.push_back(i + '0');
+        }
+
+        while(!match(num, pattern)){
+            next_permutation(begin(num), end(num));
+        }
+        return num;
+    }
+};
+int main(){
+    return 0;
+}
