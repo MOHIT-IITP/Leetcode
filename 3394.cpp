7f55c8d262aaad6460afb31b37be11ce693422e5
MOHIT-IITP <codingbymohit@gmail.com>
2025-03-25 22:51:28 +0530

    done dcc leetcode

---
+++ 3394.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  bool check(vector<vector<int>> &intervals) {
+    sort(intervals.begin(), intervals.end());
+
+    int sections = 0;
+    int max_end = intervals[0][1];
+
+    for (auto &interval : intervals) {
+      int start = interval[0], end = interval[1];
+      if (max_end <= start) {
+        sections++;
+      }
+      max_end = max(max_end, end);
+    }
+
+    return sections >= 2;
+  }
+
+  bool checkValidCuts(int n, vector<vector<int>> &rectangles) {
+    vector<vector<int>> x_intervals, y_intervals;
+
+    for (auto &rect : rectangles) {
+      x_intervals.push_back({rect[0], rect[2]});
+      y_intervals.push_back({rect[1], rect[3]});
+    }
+
+    return check(x_intervals) || check(y_intervals);
+  }
+};
+int main() { return 0; }
