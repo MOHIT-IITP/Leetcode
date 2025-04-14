611317017ca12ed04dd0f49313bf724c72ec76c8
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-04-14 22:01:09 +0530

    done dcc leetcode

---
+++ 1534.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        int countGoodTriplets(vector<int> &arr, int a, int b, int c){
+            int n = arr.size();
+            int count = 0;
+            for(int i=0;i<n;i++){
+                for(int j = i+1 ; j < n;j++){
+                    for(int k = j+1 ;k < n;k++){
+                        if(abs(arr[i] - arr[j]) <= a){
+                            if(abs(arr[j] - arr[k]) <= b){
+                                if(abs(arr[i] - arr[k]) <= c){
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+}
+
+int main(){
+    return 0;
+}
