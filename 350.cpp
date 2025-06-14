f6fa0fcdf61ea76217984d2c810b47dd291fa515
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-06-14 21:08:06 +0530

    done 350 using two pointer algo

--- 350.cpp
+++ 350.cpp
@@ -32,3 +32,58 @@ public:
 int main(){
     return 0;
 }
+
+
+
+// using binary search
+
+class Solution {
+public:
+    int binary(vector<int>& nums, int left, int right, int target) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                if (mid == left || nums[mid - 1] < target) {
+                    return mid;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+
+        if (n > m) {
+            return intersect(nums2, nums1);
+        }
+
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int st = 0;
+        vector<int> vec;
+
+        for (int i = 0; i < n; i++) {
+            int target = nums1[i];
+            int id = binary(nums2, st, m - 1, target);
+
+            if (id != -1) {
+                vec.push_back(target);
+                st = id + 1;
+            }
+        }
+
+        return vec;
+    }
+};
+
+
