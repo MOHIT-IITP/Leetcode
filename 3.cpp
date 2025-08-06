657f7247cf471062a9edf72fb656d486c818ee55
MOHIT-IITP <codingbymohit@gmail.com>
2025-08-06 18:37:08 +0530

    done: 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,29 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-  int lengthofLongestSubstring(string s){
-    vector<int> mpp(256,-1);
-    int left=0,right=0;
-    int n = s.size();
-    int len = 0;
-    while(right<n){
-      if(mpp[s[right]]!= -1){
-        left= max(mpp[s[right]]+1, left);
-      }
-      mpp[s[right]] = right;
-      len = max(len,right-left+1);
-      right ++;
-    }
-    return len;
-  }
-};
+    int lengthOfLongestSubstring(string s) {
 
-int main()
-{
-  Solution obj;
-  string s="abcabcd";
-  cout<<"Answer "<<obj.lengthofLongestSubstring(s);
-  return 0;
+        int left = 0;
+        int n = s.length();
+
+        int res = 0;
+        unordered_map<int, int> mp;
+        for(int right = 0 ;right < n; right++){
+            mp[s[right]]++;
+            for(auto const & [key, value]: mp){
+                if(value > 1){
+                    mp[s[left]]--;
+                    left++;
+                }
+            }
+            res = max(res, right - left + 1);
+        }
+        return res;
+    }
+};
+;
+
+int main(){
+    return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        int left = 0;
+        int n = s.length();
+        int res = 0;
+
+        unordered_map<int, int> mp;
+        for(int right = 0 ;right < n; right++){
+            mp[s[right]]++;
+            for(auto const & [key, value]: mp){
+                if(value > 1){
+                    mp[s[left]]--;
+                    left++;
+                }
+            }
+            res = max(res, right - left + 1);
+        }
+        return res;
+    }
+};
