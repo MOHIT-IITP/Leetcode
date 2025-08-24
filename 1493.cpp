880e354516b45d35ee24487294cf0e11e5021d45
MOHIT-IITP <codingbymohit@gmail.com>
2025-08-24 20:13:48 +0530

    done through dp

--- 1493.cpp
+++ 1493.cpp
@@ -20,3 +20,33 @@ class Solution{
         }
 }
  
+
+// dynamic programming way
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        int ans = 0;
+
+        if (nums[0] == 1) {
+            dp[0][0] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == 1) {
+                dp[i][0] = dp[i-1][0] + 1;       // no deletion
+                dp[i][1] = dp[i-1][1] + 1;       // already deleted
+            } else {
+                dp[i][0] = 0;                    // break if no deletion
+                dp[i][1] = dp[i-1][0];           // delete this 0
+            }
+            ans = max(ans, max(dp[i][0], dp[i][1]));
+        }
+
+        // Since we must delete exactly one element, answer cannot be full length
+        if (ans == n) ans--; 
+        return ans;
+    }
+};
+
