0c205490b3d51161c045a88ae3bc635915cdc2e4
MOHIT-IITP <codingbymohit@gmail.com>
2025-09-19 23:06:41 +0530

    done 48

---
+++ 46.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void solve(vector<int>& nums, vector<vector<int>>& ans, int index){
+        //base case
+        if(index>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int j=index; j<nums.size(); j++){
+            swap(nums[index], nums[j]);
+            solve(nums, ans, index+1);
+            //backtrack
+            swap(nums[index], nums[j]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int index=0;
+        solve(nums, ans, index);
+        return ans;
+    }
+};
+int main(){
+    return 0;
+}
